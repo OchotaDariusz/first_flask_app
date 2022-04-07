9d92a8e1f5437b6095b7a3f4865ab97481e29a6c
Dariusz Ochota <ochota.dariusz@gmail.com>
2022-04-07 15:20:32 +0200

    Display notes

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
-
+notes = ['first note', 'second']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', notes=notes)
 
 
 @app.route('/note', methods=['GET', 'POST'])
 def handle_notes():
-    pass
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('note.html', notes=notes)
 
 
 if __name__ == '__main__':
